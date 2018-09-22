45ea0090c1860e8ebf5801d6fc43f4ba5b635fa7
KhaledSami <k.sami.mohammed@gmail.com>
2018-09-22 16:05:56 +0100

    Add react router

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './app/index.js',
+	watch: true,
 	output:  {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.bundle.js'
+		filename: 'index.bundle.js',
+		publicPath: '/'
 	},
 	module: {
 	    rules: [
 	    	{ test: /\.(js)$/, use: 'babel-loader' },
 	    	{ test: /\.css$/, use: ['style-loader', 'css-loader']}
 	    ]
+	},
+	devServer: {
+		historyApiFallback: true
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
